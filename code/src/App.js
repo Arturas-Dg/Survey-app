1b7555f596484e64bd8dadd7c0ead1c01bdfd672
Arturas Dauginas <arturaz.dg@gmail.com>
2023-06-13 15:45:11 +0300

    Welcome page is done, adding the general look, fonts and colors of the app

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React from 'react'
+
+// const pets = [
+//   'dog',
+//   'cat',
+//   'rabbit',
+//   'fish',
+//   'snake',
+//   'guinea pig',
+//   'parrot',
+//   'hamster',
+// ]
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  );
+    <>
+      <div className="header">
+        <h1 className="welcome">Welcome!</h1>
+        <h2 className="welcome-message">
+          This is a survey on pets. If you want to proceed, press Continue.
+        </h2>
+        <button type="submit" className="continue-button">
+          Continue
+        </button>
+      </div>
+      <div className="first-question-form">
+        <h3 className="first-question">What is your favorite pet?</h3>
+      </div>
+      <input type="checkbox" id="dog" name="dog" value="dog" />
+      <label htmlFor="dog">Dog</label>
+    </>
+  )
 }
---
+++ code/src/FirstQuestion.js
@@ -0,0 +1,16 @@
+// import react from 'react'
+
+// const pets = [
+//   'dog',
+//   'cat',
+//   'rabbit',
+//   'fish',
+//   'snake',
+//   'guinea pig',
+//   'parrot',
+//   'hamster',
+// ]
+
+// export const PetSelector = () => {
+//     return ()
+// }
