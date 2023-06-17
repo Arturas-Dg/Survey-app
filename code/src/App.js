3af3d1e452694674ae2e04db229ecd794597f818
Arturas Dauginas <arturaz.dg@gmail.com>
2023-06-17 22:00:51 +0300

    downloaded pet images and added them to the website, made a working useState on pets that you like

--- code/src/App.js
+++ code/src/App.js
@@ -11,26 +11,56 @@ const pets = [
   'hamster',
 ]
 
-let [likedPet, sedLikedPet] = useState('')
+const animalPhotos = [
+  'animalPhotos/cat.jpg',
+  'animalPhotos/dog.jpg',
+  'animalPhotos/fish.jpg',
+  'animalPhotos/guinea pig.jpg',
+  'animalPhotos/hamster.jpg',
+  'animalPhotos/parrot.jpg',
+  'animalPhotos/rabbit.jpg',
+  'animalPhotos/snake.jpg',
+]
 
-const selectLikedPet = () => {}
-
-const petDisplay = pets.map((pet) => {
-  return (
-    <label className="pet-selection" htmlFor={pet} key={pet}>
-      {pet}
-      <input
-        className="checkbox"
-        type="checkbox"
-        id={pet}
-        name={pet}
-        value={pet}
-      />
-    </label>
-  )
+const displayAnimalPhotos = animalPhotos.map((animal) => {
+  return <img src={animal} className="animal-photo" alt="animal" key={animal} />
 })
 
+const scrollFirstQuestion = () => {
+  const firstQuestion = document.getElementById('first-question-form')
+  firstQuestion.scrollIntoView({
+    behavior: 'smooth',
+    block: 'end',
+    inline: 'nearest',
+  })
+}
+
 export const App = () => {
+  const [favouritePets, setFavouritePets] = useState([])
+
+  const onFavouriteChange = (e) => {
+    if (e.target.checked) {
+      setFavouritePets([...favouritePets, e.target.value])
+    } else {
+      setFavouritePets(favouritePets.filter((item) => item !== e.target.value))
+    }
+  }
+
+  const petDisplay = pets.map((pet) => {
+    return (
+      <label className="pet-selection" htmlFor={pet} key={pet}>
+        {pet}
+        <input
+          className="checkbox"
+          type="checkbox"
+          onChange={(e) => onFavouriteChange(e)}
+          id={pet}
+          name={pet}
+          value={pet}
+        />
+      </label>
+    )
+  })
   return (
     <>
       <div className="header">
@@ -38,15 +68,22 @@ export const App = () => {
         <h2 className="welcome-message">
           This is a survey on pets. If you want to proceed, press Continue.
         </h2>
-        <button type="submit" className="continue-button">
+        <button
+          type="submit"
+          className="continue-button"
+          onClick={scrollFirstQuestion}
+        >
           Continue
         </button>
       </div>
-      <div className="first-question-form">
-        <h1 className="first-question">
-          Which of these pets do you like/love?
-        </h1>
-        <div className="pet-selection-form">{petDisplay}</div>
+      <div className="first-question-form" id="first-question-form">
+        <div>
+          <h1 className="first-question">
+            Which of these pets do you like/love?
+          </h1>
+          <div className="pet-selection-form">{petDisplay}</div>
+        </div>
+        <div className="pet-images">{displayAnimalPhotos}</div>
       </div>
     </>
   )
